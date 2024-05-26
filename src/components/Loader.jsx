39abd19294bd5e88006769dbf422edd9f862060f
winterberrying <winterberrying@gmail.com>
2024-05-26 17:36:53 +0800

    update spinner loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Loader = () => {
   return (
     <div className="fixed inset-0 z-10 flex items-center justify-center">
-      <div className="loader animate-spin89345 border-4 border-gray-200 border-t-transparent rounded-full w-9 h-9"></div>
+      <span className="loading loading-spinner loading-lg"></span>
     </div>
   );
 };
